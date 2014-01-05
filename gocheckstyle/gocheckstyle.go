8532a28e423207bce4bc90026c28025e4ebfc242
longbai <slongbai@gmail.com>
2014-01-05 21:27:53 +0800

    execute file

---
+++ gocheckstyle/gocheckstyle.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"path/filepath"
+)
+
+import (
+	"github.com/longbai/checkstyle"
+)
+
+func main() {
+
+}
+
+func checkFile(fileName string) error {
+
+}
+
+func checkDir(dir string) error {
+
+}
