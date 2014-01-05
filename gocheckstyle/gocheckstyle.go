560644f05f773d88148ba6f2c7e9a66c25292955
longbai <slongbai@gmail.com>
2014-01-05 21:53:48 +0800

    runner

--- gocheckstyle/gocheckstyle.go
+++ gocheckstyle/gocheckstyle.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"path/filepath"
 )
 
@@ -10,14 +12,54 @@ import (
 	"github.com/longbai/checkstyle"
 )
 
+var gConfig []byte
+
 func main() {
+	config := flag.String("config", "", "config json file")
 
+	flag.Parse()
+
+	files := flag.Args()
+	for _, v := range files {
+		if isDir(v) {
+			checkDir(v)
+		} else {
+			checkFile(v)
+		}
+	}
 }
 
-func checkFile(fileName string) error {
-
+func isDir(filename string) bool {
+	fi, err := os.Stat(filename)
+	return err == nil && fi.IsDir()
 }
 
-func checkDir(dir string) error {
+func checkFile(fileName string) {
+	file, err := ioutil.ReadAll(fileName)
+	if err != nil {
+		log.Printf("Read File Fail %v %v", fileName, err)
+	}
 
+	checker, err := checkstyle.New(gConfig)
+	if err != nil {
+		panic("New Checker Fail")
+	}
+
+	ps, err := checker.Check(fileName, file)
+	if err != nil {
+		log.Printf("Parse File Fail %v %v", fileName, err)
+	}
+
+	for _, v := range ps {
+		fmt.Printf("%s:%v: %s\n", filename, p.Position, p.Description)
+	}
+}
+
+func checkDir(dir string) {
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err == nil && !info.IsDir() && strings.HasSuffix(path, ".go") {
+			checkFile(path)
+		}
+		return err
+	})
 }
