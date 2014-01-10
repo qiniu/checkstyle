e9ced52fe355d5787cbc02d9e5cc042a6ef9b93d
longbai <slongbai@gmail.com>
2014-01-10 17:19:40 +0800

    test and config

---
+++ checkstyle_test.go
@@ -0,0 +1,16 @@
+package checkstyle
+
+import (
+	// "io/ioutil"
+	"testing"
+)
+
+func TestFileLine(t *testing.T) {
+	// baseDir := "testdata/"
+	// file, err := ioutil.ReadFile(baseDir + "fileline.go")
+	// _checkerOk := checker{FileLine: 50}
+	// ps, err := _checkerOk.Check("fileline.go", file)
+	//    if err !=  {
+
+	//    }
+}
